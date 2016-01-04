930bd676969b30970fc9b7a80af3ce0cc0459ea5
Pratik Chavan <pratik.chavan@gmail.com>
2016-01-04 18:20:48 -0500

    Updates to server side logic

--- app/static/index.js
+++ app/static/index.js
@@ -35,21 +35,11 @@ google.maps.event.addDomListener(window, "load", function () {
   /*
    * add markers to map
    */
-  var marker0 = createMarker({
-    position: new google.maps.LatLng(33.808678, -117.918921),
-    map: map,
-    icon: "http://1.bp.blogspot.com/_GZzKwf6g1o8/S6xwK6CSghI/AAAAAAAAA98/_iA3r4Ehclk/s1600/marker-green.png"
-  }, "<h1>Marker 0</h1><p>This is the home marker.</p>");
-
-  var marker1 = createMarker({
-    position: new google.maps.LatLng(33.818038, -117.928492),
-    map: map
-  }, "<h1>Marker 1</h1><p>This is marker 1</p>");
-
-  var marker2 = createMarker({
-    position: new google.maps.LatLng(33.803333, -117.915278),
-    map: map
-  }, "<h1>Marker 2</h1><p>This is marker 2</p>");
+  //var marker0 = createMarker({
+  //  position: new google.maps.LatLng(33.808678, -117.918921),
+  //  map: map,
+  //  icon: "http://1.bp.blogspot.com/_GZzKwf6g1o8/S6xwK6CSghI/AAAAAAAAA98/_iA3r4Ehclk/s1600/marker-green.png"
+  //}, "<h1>CALI SIDE</h1><p>Los Angeles.</p>");
 });
 
 // Locate me method
@@ -80,14 +70,44 @@ function findPlace() {
         location_data['lat'] = split_location[0].replace("(", "");
         location_data['long'] = split_location[1].replace(")", "");
         console.log("split_locationn " + split_location + location_data['lat']);
-
+        var marker1=null;
         var a = $.ajax({
             type : "POST",
             url : "/data_to_find_distance",
             data: JSON.stringify(location_data),
             contentType: 'application/json;charset=UTF-8;',
             success: function(result) {
-                console.log(""+result);
+                console.log(result);
+
+                var infoWindow = new google.maps.InfoWindow();
+
+  /*
+   * marker creater function (acts as a closure for html parameter)
+   */
+                function createMarker(options, html) {
+                    var marker = new google.maps.Marker(options);
+                    if (html) {
+                        google.maps.event.addListener(marker, "click", function () {
+                            infoWindow.setContent(html);
+                            infoWindow.open(options.map, this);
+                        });
+                    }
+                    return marker;
+                }
+
+                var parsedResult = JSON.parse(result);
+                for (var key in parsedResult) {
+                    console.log("Key = ",key," val = ",parsedResult[key][0],parsedResult[key][1]);
+                    var lat = parseFloat(parsedResult[key][0]);
+                    var lon = parseFloat(parsedResult[key][1]);
+                    var marker1 = createMarker({
+                        position: new google.maps.LatLng(lat, lon),
+                        map: map,
+                        icon: "http://1.bp.blogspot.com/_GZzKwf6g1o8/S6xwK6CSghI/AAAAAAAAA98/_iA3r4Ehclk/s1600/marker-green.png"
+                        }, "<h1>"+key+"</h1><p></p>");
+                    console.log("Marker added for ", key);
+                }
+                map.setZoom(17);
             }
         });
         console.log(a);
