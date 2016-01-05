d2e06d5571aac7f48e64a958aa09869a5d3552bd
Gaurav Kshirsagar <gauravk1001@gmail.com>
2016-01-05 02:23:43 -0500

    Fixed map pins

--- app/static/index.js
+++ app/static/index.js
@@ -1,5 +1,6 @@
 var map;
 var location_data;
+var allMarkers=[];
 /*
  * use google maps api built-in mechanism to attach dom events
  */
@@ -33,14 +34,6 @@ google.maps.event.addDomListener(window, "load", function () {
         return marker;
     }
 
-  /*
-   * add markers to map
-   */
-  //var marker0 = createMarker({
-  //  position: new google.maps.LatLng(33.808678, -117.918921),
-  //  map: map,
-  //  icon: "http://1.bp.blogspot.com/_GZzKwf6g1o8/S6xwK6CSghI/AAAAAAAAA98/_iA3r4Ehclk/s1600/marker-green.png"
-  //}, "<h1>CALI SIDE</h1><p>Los Angeles.</p>");
 });
 
 // Locate me method
@@ -60,7 +53,7 @@ function findPlace() {
                 map:map,
                 position:results[0].geometry.location
             });
-            console.log("Result as is" + results[0].geometry.location);
+            // console.log("Result as is" + results[0].geometry.location);
         }
         else{
             alert('address not found!');
@@ -71,7 +64,7 @@ function findPlace() {
         location_data = {};
         location_data['lat'] = split_location[0].replace("(", "");
         location_data['long'] = split_location[1].replace(")", "");
-        console.log("split_locationn " + split_location + location_data['lat']);
+        // console.log("split_locationn " + split_location + location_data['lat']);
         var marker1=null;
         var a = $.ajax({
             type : "POST",
@@ -80,39 +73,9 @@ function findPlace() {
             contentType: 'application/json;charset=UTF-8;',
             success: function(result) {
                 console.log(result);
-
-                var infoWindow = new google.maps.InfoWindow();
-
-  /*
-   * marker creater function (acts as a closure for html parameter)
-   */
-                function createMarker(options, html) {
-                    var marker = new google.maps.Marker(options);
-                    if (html) {
-                        google.maps.event.addListener(marker, "click", function () {
-                            infoWindow.setContent(html);
-                            infoWindow.open(options.map, this);
-                        });
-                    }
-                    return marker;
-                }
-
-                var parsedResult = JSON.parse(result);
-                for (var key in parsedResult) {
-                    console.log("Key = ",key," val = ",parsedResult[key][0],parsedResult[key][1]);
-                    var lat = parseFloat(parsedResult[key][0]);
-                    var lon = parseFloat(parsedResult[key][1]);
-                    var marker1 = createMarker({
-                        position: new google.maps.LatLng(lat, lon),
-                        map: map,
-                        icon: "http://1.bp.blogspot.com/_GZzKwf6g1o8/S6xwK6CSghI/AAAAAAAAA98/_iA3r4Ehclk/s1600/marker-green.png"
-                        }, "<h1>"+key+"</h1><p></p>");
-                    console.log("Marker added for ", key);
-                }
-                map.setZoom(15);
+                plotMarkers(result);
             }
         });
-        console.log(a);
     });
 }
 
@@ -127,7 +90,7 @@ function findMyLocation(){
 }
 
 function showPosition(position) {
-    alert(parseFloat(position.coords.latitude) + " " +  parseFloat(position.coords.longitude));
+    // alert(parseFloat(position.coords.latitude) + " " +  parseFloat(position.coords.longitude));
     location_data = {};
     location_data['lat'] = position.coords.latitude;
     location_data['long'] = position.coords.longitude;
@@ -138,6 +101,11 @@ function showPosition(position) {
         contentType: 'application/json;charset=UTF-8;',
         success: function(result) {
             console.log("" + result);
+            while(allMarkers.length>0){
+                var tempMarker = allMarkers.pop();
+                tempMarker.setMap(null);
+            }
+            plotMarkers(result);
         }
     });
 
@@ -159,6 +127,46 @@ function filterResults() {
         contentType: 'application/json;charset=UTF-8;',
         success: function(result) {
             console.log("" + result);
+            while(allMarkers.length>0){
+                var tempMarker = allMarkers.pop();
+                tempMarker.setMap(null);
+            }
+            plotMarkers(result);
         }
     });
+}
+
+
+function plotMarkers(result){
+
+    var infoWindow = new google.maps.InfoWindow();
+
+/*
+* marker creater function (acts as a closure for html parameter)
+*/
+    function createMarker(options, html) {
+        var marker = new google.maps.Marker(options);
+        if (html) {
+            google.maps.event.addListener(marker, "click", function () {
+                infoWindow.setContent(html);
+                infoWindow.open(options.map, this);
+            });
+        }
+        return marker;
+    }
+
+    var parsedResult = JSON.parse(result);
+    for (var key in parsedResult) {
+        // console.log("Key = ",key," val = ",parsedResult[key][0],parsedResult[key][1]);
+        var lat = parseFloat(parsedResult[key][0]);
+        var lon = parseFloat(parsedResult[key][1]);
+        var marker1 = createMarker({
+            position: new google.maps.LatLng(lat, lon),
+            map: map,
+            icon: "http://1.bp.blogspot.com/_GZzKwf6g1o8/S6xwK6CSghI/AAAAAAAAA98/_iA3r4Ehclk/s1600/marker-green.png"
+        }, "<h6>"+key+"</h6>");
+        allMarkers.push(marker1);
+        // console.log("Marker added for ", key);
+    }
+    map.setZoom(16);
 }
--- app/views.py
+++ app/views.py
@@ -11,7 +11,7 @@ def index():
 @app.route('/data_to_find_distance', methods = ["GET", "POST"])
 def data_to_find_distance():
     if request.method == "POST":
-        print 'In url call ', request.json
+        # print 'In url call ', request.json
         if len(request.json) == 4:
             return calculate_distance(request.json, radius = request.json['radius'], number_of_results = request.json['results_limit'])
         else:
